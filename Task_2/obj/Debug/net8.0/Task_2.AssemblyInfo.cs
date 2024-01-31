6d6d30a653407837c2179b11e6ee00b23877b134
Grigor_Ina <grigorina72@gmail.com>
2024-01-31 18:04:31 +0200

    Sarcina_3

--- Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
+++ Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f0930cab2f3479757b661bcf958b00872d3734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76e6edadad0a92ce889950e3b36f755f9584c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
