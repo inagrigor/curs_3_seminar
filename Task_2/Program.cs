75f0930cab2f3479757b661bcf958b00872d3734
Grigor_Ina <grigorina72@gmail.com>
2024-01-31 16:16:56 +0200

    sarcina_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -3,8 +3,22 @@
 // Примеры 
 // a = 256 => 26 
 // a = 891 => 81
-int number = 891; 
-int firstDigit = number /  100;
-int thirdDigit = number % 10; 
-int result = firstDigit * 10 + thirdDigit;
-Console.WriteLine($" cifra {number} se transforma in {result}");
+// int number = 891;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int result = firstDigit * 10 + thirdDigit;
+// Console.WriteLine($" cifra {number} se transforma in {result}");
+
+// Напишите программу, которая принимает на вход трёхзначное число и 
+// возводит вторую цифру этого числа в степень, равную третьей цифре. 
+// Примеры 
+// 487 => 8^7 = 2 097 152 
+// 254 => 5^4 = 625 
+// 617 => 1
+// int number = 617;
+// int firstDigit = number / 100;
+// int secondDigit = number / 10 % 10;
+// int thirdDigit = number % 10;
+// int result = (int)Math.Pow(secondDigit, thirdDigit);
+
+// Console.WriteLine($" {secondDigit} ^ {thirdDigit} = {result}");
--- Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
+++ Task_2/obj/Debug/net8.0/Task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f7959164b202dc5df074b33e8e8711c8361b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a911929fbcb2e5f7edba9fa40a68fc141bb93a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
