a911929fbcb2e5f7edba9fa40a68fc141bb93a48
Grigor_Ina <grigorina72@gmail.com>
2024-01-31 15:43:47 +0200

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//Напишите программу, которая принимает на вход  трёхзначное число и удаляет вторую цифру этого числа.
+// Примеры 
+// a = 256 => 26 
+// a = 891 => 81
+int number = 891; 
+int firstDigit = number /  100;
+int thirdDigit = number % 10; 
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($" cifra {number} se transforma in {result}");
