eba7f1e805f17d5073f993bdddd88164e5281c21
Grigor_Ina <grigorina72@gmail.com>
2024-02-11 07:30:32 +0200

    ceva cu tema de acasa_2

--- Tema_acasa_2/obj/Debug/net8.0/Tema_acasa_2.AssemblyInfo.cs
+++ Tema_acasa_2/obj/Debug/net8.0/Tema_acasa_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tema_acasa_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa296f05a429bca7fb32f63236111ee4aece75bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bba242a4fdb810723ba5e75fe91735baad90630")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tema_acasa_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tema_acasa_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
