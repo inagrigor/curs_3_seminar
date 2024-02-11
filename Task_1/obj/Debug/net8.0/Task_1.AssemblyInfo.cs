e2d27aa6350a9e20f393fcf9b9836e214645b088
Grigor_Ina <grigorina72@gmail.com>
2024-02-11 07:28:49 +0200

    ceva cu Task_1

--- Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
+++ Task_1/obj/Debug/net8.0/Task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa296f05a429bca7fb32f63236111ee4aece75bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bba242a4fdb810723ba5e75fe91735baad90630")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
