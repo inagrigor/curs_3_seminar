978b76e01cfad2f3140e21cc1d5b2462e94b9f7d
Grigor_Ina <grigorina72@gmail.com>
2024-02-11 12:59:51 +0200

    sarcina 2

--- seminar_4/Program.cs
+++ seminar_4/Program.cs
@@ -61,3 +61,37 @@
 // // int count = GetCount(array);
 // // Console.WriteLine(count);
 // Console.WriteLine($"Result: {GetCount(array)}");
+
+// Sarcina 2
+// Заполните массив на  N (вводится с консоли, не более 8) случайных целых чисел от 0 до 9. 
+// Сформируйте целое число, которое будет состоять из цифр из массива.
+//  Старший разряд числа находится на 0-м индексе, младший – на последнем. 
+// Пример [1 3 2 4 2 3] => 132423 [2 3 1] => 231
+
+// int[] CreatArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(10);
+//     }
+//     return array;
+// }
+// int ConvertArrayToInteger(int[] array)
+// {
+//     int result = 0;
+//     for (int i = 0, j = array.Length - 1; i < array.Length; i++, j-- )
+//     {
+//         result = result + array[i] * (int)Math.Pow(10, j);
+//     }
+//     return result;
+// }
+
+
+
+
+// Console.WriteLine("introduceti numarul: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreatArray(N);//creat un massiv din elemente N
+// Console.WriteLine($"Masiv: [ {string.Join("; ", array)} ]");
+// Console.WriteLine($"Result: {ConvertArrayToInteger(array)}");
