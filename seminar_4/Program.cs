7951ed57e6db52afd86ad885a604248896f69444
Grigor_Ina <grigorina72@gmail.com>
2024-03-19 08:00:07 +0200

    ceva seminar _4

--- seminar_4/Program.cs
+++ seminar_4/Program.cs
@@ -58,7 +58,7 @@
 // Console.WriteLine("introduceti numarul: ");
 // int N = Convert.ToInt32(Console.ReadLine());
 // int[] array = CreatArray(N);
-// Console.WriteLine($"Masiv: [ {string.Join("; ", array)} ]");
+// Console.WriteLine($"Masiv: [ {string.Join("; ", array)} ]")
 // // int count = GetCount(array);
 // // Console.WriteLine(count);
 // Console.WriteLine($"Result: {GetCount(array)}");
@@ -96,3 +96,79 @@
 // int[] array = CreatArray(N);//creat un massiv din elemente N
 // Console.WriteLine($"Masiv: [ {string.Join("; ", array)} ]");
 // Console.WriteLine($"Result: {ConvertArrayToInteger(array)}");
+
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый 
+// массив из строк, длина которых меньше, либо равна 3 символам.
+//  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+//[“Hello”, “2”, “world”, “:-0”] → [“2”, “:-0”]
+
+// string[] ConvertStringArray(string[] array)
+// {
+//     int count = 0;
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             count++;
+//         }
+//     }
+//     string[] arrs = new string[count];
+//     int j = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             arrs[j] = array[i];
+//             j++;
+//         }
+//     }
+//     return arrs;
+// }
+
+
+// string[] array = { "Hello", "2", "world", ":-0" };
+// string[] arrs = ConvertStringArray(array);
+// Console.WriteLine($"masiv: [{string.Join("; ", arrs)}]");
+
+
+// //[“2”, “:-0”]
+// //Console.WriteLine($"Masiv: [ {string.Join("; ", array)} ]")
+
+
+// int GetCountStringInArray(string[] array)
+// {
+//     int countStrings = 0;
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             countStrings++;
+//         }
+//     }
+//     Console.WriteLine(countStrings);
+//     return countStrings;
+// }
+// string[] ConvertStringArray(string[] array)
+// {
+//     string[] arrs = new string[countStrings];
+//     int j = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] <= 3)
+//         {
+//             arrs[j] = array[i];
+//             j++;
+//         }
+//     }
+//     return arrs;
+// }
+
+
+// string[] array = { "Hello", "2", "world", ":-0" };
+// string[] arrs = ConvertStringArray(array);
+// Console.WriteLine($"masiv: [{string.Join("; ", arrs)}]");
